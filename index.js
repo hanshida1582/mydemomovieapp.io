0afb84be17d5393db942c783287bf058fb1b049a
hanshida1582 <114293357+hanshida1582@users.noreply.github.com>
2024-02-17 15:09:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from './App';
+
+// React 17
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(, document.getElementById("root"));
+
+// React 18:
+//import React from "react";
+//import ReactDOM from "react-dom/client";
+//import App from "./App";
+
+//ReactDOM.createRoot(document.getElementById("root")).render();
+ReactDOM.render(<App />, document.getElementById('root'));
+//import { createRoot } from 'react-dom';
+
+//createRoot(document.getElementById('root')).render(<App />);
+
+
+
+
